0e29c4dbe2c3abb772f083a995f04ee362ab70a5
Kenny Lam <kenny.lam@mail.utoronto.ca>
2017-03-08 02:36:56 -0500

    added popup settings menu and tooltip

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -5,42 +5,125 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import { blueGrey100, blueGrey500 } from 'material-ui/styles/colors';
+import Popover from 'material-ui/Popover/Popover';
+import { Menu, MenuItem } from 'material-ui/Menu';
+import ReactTooltip from 'react-tooltip'
 import './App.css';
+
+const Settings = ({anchorEl, anchorOrigin, targetOrigin, handleCloseSettings, open}) => {
+  return (
+    <div>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={anchorOrigin}
+        targetOrigin={targetOrigin}
+        onRequestClose={handleCloseSettings}
+      >
+        <Menu>
+          <MenuItem primaryText="Help &amp; feedback" />
+          <MenuItem primaryText="Settings" />
+          <MenuItem primaryText="Sign out" />
+        </Menu>
+      </Popover>
+    </div>
+  );
+};
+
+const WhatsThis = () => {
+  return (
+    <div>
+      <label className="popup-whats-this" data-tip data-for='whats-this-tooltip'>What's this?</label>
+      <ReactTooltip id='whats-this-tooltip' place='top' effect='solid'>
+        <div className="whats-this-text">
+          This extension requires you to authenticate with your GitHub account
+          ONLY for project issue management.
+        </div>
+      </ReactTooltip>
+    </div>
+  );
+};
 
 class App extends Component {
 
-  openGithubAuth = () => {
+ constructor(props) {
+    super(props);
+
+    this.anchorOrigin = {
+      horizontal: 'right',
+      vertical: 'bottom',
+    };
+
+    this.targetOrigin = {
+      horizontal: 'right',
+      vertical: 'top',
+    };
+
+    this.state = {
+      open: false,
+    };
+  };
+
+  handleOpenSettings = (event) => {
+    event.preventDefault();
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleCloseSettings = () => {
+    this.setState({
+      open: false,
+        });
+  };
+
+  handleOpenGithubAuth = () => {
     chrome.tabs.create({ 'url': process.env.REACT_APP_SERVER_ROUTE + '/api/auth/github' }, function (tab) {
       // Tab opened.
     });
   };
 
-  openWhatsThis = () => {
-    
-  };
-
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="popup">
-          <div className="popup-settings-bar">
-            <ActionSettings color={ blueGrey500 } hoverColor={ blueGrey100 } />
+      <div>
+        <MuiThemeProvider>
+          <div className="popup">
+            <div className="popup-settings-bar">
+              <div className="settings">
+                <ActionSettings
+                  id="popup-settings"
+                  color={ blueGrey500 }
+                  hoverColor={ blueGrey100 }
+                  onClick={this.handleOpenSettings}
+                />
+              </div>
+            </div>
+            <div className="popup-header">
+              <div className="popup-title">KanHub</div>
+            </div>
+            <div className="popup-body">
+              <RaisedButton
+                primary={true}
+                label="Authenticate with GitHub"
+                onClick={this.handleOpenGithubAuth}
+              />
+            </div>
+            <div className="popup-footer">
+              <WhatsThis />
+            </div>
           </div>
-          <div className="popup-header">
-            <div className="popup-title">KanHub</div>
-          </div>
-          <div className="popup-body">
-            <RaisedButton
-              primary={true}
-              label="Authenticate with GitHub"
-              onClick={this.openGithubAuth}
-            />
-          </div>
-          <div className="popup-footer">
-            <a target="_blank" href={process.env.REACT_APP_SERVER_ROUTE + '/api/auth/github'}>What's this?</a>
-          </div>
-        </div>
-      </MuiThemeProvider>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Settings
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={this.anchorOrigin}
+            targetOrigin={this.targetOrigin}
+            handleCloseSettings={this.handleCloseSettings}
+            open={this.state.open}
+          />
+        </MuiThemeProvider>
+      </div>
     );
   };
 }
