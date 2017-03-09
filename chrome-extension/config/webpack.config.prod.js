6417d934285111a040f2092c36e7fcee6d6facd0
Kenny Lam <kenny.lam@mail.utoronto.ca>
2017-03-09 00:59:07 -0500

    fixed entry points

--- chrome-extension/config/webpack.config.prod.js
+++ chrome-extension/config/webpack.config.prod.js
@@ -52,11 +52,10 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
-  entry: [
-    require.resolve('./polyfills'),
-    paths.appIndexJs,
-    paths.appSettingsJs
-  ],
+  entry: {
+    index: [require.resolve('./polyfills'), paths.appIndexJs],
+    settings: paths.appSettingsJs,
+  },
   output: {
     // The build folder.
     path: paths.appBuild,
