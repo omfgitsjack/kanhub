45610b415a078d1a2ac94d11efb449a8e22f71ce
Jack Yiu <jackinyiu@gmail.com>
2017-03-26 23:05:18 -0400

    Updated landing page to have the verification

--- server/src/front-end/index.js
+++ server/src/front-end/index.js
@@ -1,11 +1,17 @@
+var url = "https://chrome.google.com/webstore/detail/peacnodjjlhhodekaiafamddcgpjnbdo"
+
 window.onload = function() {
     var showInstructions = false;
     
     document.getElementById('download').onclick = function(e) {
         showInstructions = true;
 
-        if (showInstructions) {
-            document.getElementById('instructions').style.display = "block";
-        }
+        chrome.webstore.install(url, function() {
+            if (showInstructions) {
+                document.getElementById('instructions').style.display = "block";
+            }    
+        }, function(err) {
+            console.log(err)
+        })
     }
 }
